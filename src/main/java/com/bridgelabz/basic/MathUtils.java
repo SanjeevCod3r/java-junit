99a1331205a044e3eeb9f4f439cd634dac59b3c9
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-04-06 17:20:19 +0530

    [Sanjeev Jaiswal] ADD: Added division method with exception handling for divide by zero

---
+++ src/main/java/com/bridgelabz/basic/MathUtils.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.basic;
+
+public class MathUtils {
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
