f00473d3ba174f69e19f48da4d7e304c48b7685e
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-04-06 17:20:20 +0530

    [Sanjeev Jaiswal] ADD: Setup DatabaseConnection class with connect and disconnect methods

---
+++ src/main/java/com/bridgelabz/basic/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.basic;
+
+
+public class DatabaseConnection {
+
+    private boolean connected = false;
+
+    public void connect() {
+        System.out.println("Connecting to database...");
+        connected = true;
+    }
+
+    public void disconnect() {
+        System.out.println("Disconnecting from database...");
+        connected = false;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+}
